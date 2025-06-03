0004e404409ed062fa389024da454533db6711ea
Markoccini <mark.lukaj99@gmail.com>
2025-06-03 13:37:23 +0200

    Implement custom Exceptions

--- src/main/java/com/markoccini/toolkit/poll/service/PollService.java
+++ src/main/java/com/markoccini/toolkit/poll/service/PollService.java
@@ -1,4 +1,15 @@
 package com.markoccini.toolkit.poll.service;
 
 public class PollService {
+        try {
+            if (poll == null) {
+                throw new PollNotFoundException("Poll not found.");
+            }
+            if (poll.isClosed()) {
+                throw new PollClosedException("Poll is already closed!");
+            }
+        catch (Exception e) {
+            System.out.println(e.getMessage()); // TODO: Replace by proper handling
+        }
+    }
 }
