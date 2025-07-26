0ba55cdf9bac72008a7eba57be38abca65617d2e
Markoccini <mark.lukaj99@gmail.com>
2025-07-26 23:37:40 +0200

    ADD: Basic Pollservice functions

--- src/main/java/org/markoccini/toolkit/poll/service/PollService.java
+++ src/main/java/org/markoccini/toolkit/poll/service/PollService.java
@@ -1,6 +1,6 @@
 package org.markoccini.toolkit.poll.service;
 
-import org.markoccini.toolkit.poll.dto.PollResponse;
+import org.markoccini.toolkit.poll.model.Choice;
 import org.markoccini.toolkit.poll.model.Poll;
 import org.markoccini.toolkit.poll.repository.ChoiceRepository;
 import org.markoccini.toolkit.poll.repository.PollRepository;
@@ -21,4 +21,50 @@ public class PollService {
         return pollRepository.findAll();
     }
 
+    public Poll getPollById(long id) {
+        return pollRepository.findById(id).orElse(null);
+    }
+
+    public Poll createPoll(Poll poll) {
+        return pollRepository.save(poll);
+    }
+
+    public Poll closePoll(Poll poll) {
+        poll.closePoll();
+        return pollRepository.save(poll);
+    }
+
+    public Poll removeChoice(Poll poll, Choice choice) {
+        poll.removeChoice(choice);
+        return  pollRepository.save(poll);
+    }
+
+    public Poll addChoice(Poll poll, Choice choice) {
+        poll.addChoice(choice);
+        return pollRepository.save(poll);
+    }
+
+    public Poll editQuestion(Poll poll, String question) {
+        poll.setQuestion(question);
+        return pollRepository.save(poll);
+    }
+
+    public void deletePoll(long id) {
+        pollRepository.deleteById(id);
+    }
+
+    public Choice changeChoice(Choice choice, String new_content) {
+        choice.setContent(new_content);
+        return choiceRepository.save(choice);
+    }
+
+    public Choice voteForChoice(Choice choice) {
+        choice.incrementVotes();
+        return choiceRepository.save(choice);
+    }
+
+    public Choice unvoteForChoice(Choice choice) {
+        choice.decrementVotes();
+        return choiceRepository.save(choice);
+    }
 }
