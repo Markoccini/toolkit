b5c1284841f0fa4975b24565a2cbce565b5030ec
Markoccini <mark.lukaj99@gmail.com>
2025-08-08 02:05:21 +0200

    ADD: Tags to Routes for Swagger UI

--- src/main/java/org/markoccini/toolkit/poll/controller/PollController.java
+++ src/main/java/org/markoccini/toolkit/poll/controller/PollController.java
@@ -1,14 +1,11 @@
 package org.markoccini.toolkit.poll.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.markoccini.toolkit.poll.dto.PollRequest;
 import org.markoccini.toolkit.poll.dto.PollResponse;
-import org.markoccini.toolkit.poll.model.Choice;
-import org.markoccini.toolkit.poll.model.Poll;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import org.markoccini.toolkit.poll.service.PollService;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,18 +16,22 @@ public class PollController {
 
     public PollController(PollService pollService) {
         this.pollService = pollService;
+
     }
 
+    @Operation(tags = {"GET"})
     @GetMapping("")
     public List<PollResponse> getPolls() {
         return pollService.getAllPolls();
     }
 
+    @Operation(tags = {"GET"})
     @GetMapping("/{pollId}")
     public PollResponse getPoll(@PathVariable("pollId") Long pollId) throws Exception {
         return pollService.getPollById(pollId);
     }
 
+    @Operation(tags = {"POST"})
     @PostMapping
     public PollResponse createPoll(@RequestBody PollRequest pollRequest) {
         return pollService.createPoll(pollRequest);
