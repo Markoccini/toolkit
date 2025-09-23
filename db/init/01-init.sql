3a3f3608be525620a3f9de58dfe57b4adfd1c71a
Markoccini <mark.lukaj99@gmail.com>
2025-09-23 23:13:19 +0200

    move db secrets into env files

--- db/init/01-init.sql
+++ db/init/01-init.sql
@@ -1,1 +1,12 @@
-CREATE SCHEMA IF NOT EXISTS polls;
+-- Create Schema
+
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+       SELECT 1 FROM information_schema.schemata WHERE schema_name = 'polls'
+   ) THEN
+       EXECUTE 'CREATE SCHEMA polls AUTHORIZATION ' || current_user;
+END IF;
+END
+$$;
