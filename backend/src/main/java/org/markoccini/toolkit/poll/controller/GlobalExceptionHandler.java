b4591e1c594847bc605aa0f3f0a4d67806b587d0
Markoccini <mark.lukaj99@gmail.com>
2025-11-12 22:08:56 +0100

    Add GlobalExceptionHandler for the custom Exceptions

---
+++ backend/src/main/java/org/markoccini/toolkit/poll/controller/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package org.markoccini.toolkit.poll.controller;
+
+import org.markoccini.toolkit.common.exceptions.BadRequestException;
+import org.markoccini.toolkit.common.exceptions.DatabaseException;
+import org.markoccini.toolkit.common.exceptions.NotFoundException;
+import org.markoccini.toolkit.common.exceptions.ServerErrorException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({NotFoundException.class})
+    public ResponseEntity<Map<String, String>> handleNotFoundException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler({
+            BadRequestException.class
+    })
+    public ResponseEntity<Map<String, String>> handleBadRequestExceptions(Exception ex) {
+        return  ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler({
+            ServerErrorException.class,
+    })
+    public ResponseEntity<Map<String, String>> handleServerErrorExceptions(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler({
+            DatabaseException.class,
+    })
+    public ResponseEntity<Map<String, String>> handleDatabaseExceptions(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", ex.getMessage()));
+    }
+}
