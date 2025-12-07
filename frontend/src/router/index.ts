a2a4e916db4d93a4ef60664d24ff1722e86850a1
Markoccini <mark.lukaj99@gmail.com>
2025-12-07 01:43:58 +0100

    Modify: routing paths to specify Poll service

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Polls from '../components/Polls.vue'
+import PollList from '../components/Polls/PollList.vue'
+import PollForm from '../components/Polls/PollForm.vue'
 
 const routes: RouteRecordRaw[] = [
-    { path: '/', component: Polls },
+    { path: '/polls', component: PollList },
+    { path: '/test', component: PollForm },
 ]
 
 export const router = createRouter({
